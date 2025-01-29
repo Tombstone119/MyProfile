2898d54d0dad0294fe022241518be105eb2fa16a
Tombstone119 <kodagodayohan@gmail.com>
2025-01-29 17:09:27 +0530

    chor: making the gird component

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
-import MagicButton from "./ui/MagicButton"
+import MagicButton from "./ui/MagicButton";
 import { FaLocationArrow } from "react-icons/fa6";
 
 const Hero = () => {
@@ -19,15 +19,13 @@ const Hero = () => {
         <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-
       <div className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black/[0.2] absolute top-0 left-0 flex items-center justify-center">
-    
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"/>
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
-      <div className="max-W-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <h2 className="uppercase tracking-widest text-ws text-center text-blue-100 max-w-80 ">
+        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+          <h2 className="uppercase tracking-widest text-ws text-center text-blue-100 max-w-80">
             Dynamic Web Design With Yohan
           </h2>
           <TextGenerateEffect
@@ -35,18 +33,17 @@ const Hero = () => {
             words="This is a test build"
           />
           <p className="text-center text-[40px] md:text-5xl lg:text-6xl">
-            Hi I&apos;m Yohan a next js dev based in Sri Lanka 
+            Hi I&apos;m Yohan, a Next.js dev based in Sri Lanka.
           </p>
           <a href="#about">
             <MagicButton
-             title="show my work"
-             icon={<FaLocationArrow/>}
-             position="right"
-             />
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
           </a>
         </div>
       </div>
-
     </div>
   );
 };
